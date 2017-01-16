85d180c3da91d1768dfb42bdbeb4a7a1cb25a277
joel T <joel.torib@gmail.com>
2017-01-16 16:26:24 +0100

    Utilsation un filtre --> [explication PIPE ]

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ angular.module('app', [])
 
 .controller('MyFirstController',class MyFirstController {
     constructor() {
-        this.image = 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Male_mallard_standing.jpg/220px-Male_mallard_standing.jpg';
+        this.dte = new Date();
     }
+})
+
+.filter('toto', function () {
+    return (input) => input.split('').reverse().join('');
 })
