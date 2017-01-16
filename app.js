a9f695678a1c4b04c49684ec80658bc66be1f7c8
joel T <joel.torib@gmail.com>
2017-01-16 14:41:29 +0100

    Classe passé dans le constructeur

--- app.js
+++ app.js
@@ -5,22 +5,7 @@ angular.module('app', [])
 })
 
 .controller('MyFirstController',class MyFirstController {
-
     constructor() {
-        const u1 = { name: 'Thomas' };
-        const u2 = { name: 'Georges'}; 
-        this.users = [u1, u2];
-    }
-
-    startWithT(user) { 
-        return user.name[0] === 'T';
-    }
-
-    filterUsers() {
-        this.users.filter(this.startWithT);
-    }
-
-    show(user) {
-        user.show = true;
+        this.cssClass = 'joli';
     }
 })
