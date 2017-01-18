97f4c4805506b933e0776ee1853446188cd19df1
joel T <joel.torib@gmail.com>
2017-01-18 11:52:13 +0100

    Ajout d'une page quand on clique sur un user

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import ngRoute from 'angular-route';
 import { ExclamationFilter } from './exclamation.filter';
 import { EldestFilter } from './eldest.filter';
 import { MyFirstController } from './my-first.controller';
+import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 // Permet de faire dependre toute l'applicaiton de ce module
@@ -14,6 +15,7 @@ angular.module('app', [
 
 
 .controller('MyFirstController', MyFirstController)
+.controller('UserController', UserController)
 .filter('exclamation', ExclamationFilter)
 .filter('eldest', EldestFilter)
 .service('UserService', UserService)
@@ -33,6 +35,14 @@ angular.module('app', [
       templateUrl: 'users.html',
       controller: 'MyFirstController',
       controllerAs: 'ctrl'
+    })
+   .when('/user/:id', {
+      templateUrl: 'user.html',
+      controller: 'UserController',
+      controllerAs: 'ctrl'
+    })
+      .otherwise({
+      redirectTo: '/'
     });
 
 })
