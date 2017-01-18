7261f6124152b51ce76965d07db071497672b79c
joel T <joel.torib@gmail.com>
2017-01-18 10:44:39 +0100

    init 1 route Bienvenue routage de application

--- src/app.js
+++ src/app.js
@@ -1,18 +1,33 @@
 import angular from 'angular';
+// Nouveau module
+import ngRoute from 'angular-route';
 
 import { ExclamationFilter } from './exclamation.filter';
 import { EldestFilter } from './eldest.filter';
 import { MyFirstController } from './my-first.controller';
 import { UserService } from './user.service';
 
-angular.module('app', [])
+// Permet de faire dependre toute l'applicaiton de ce module
+angular.module('app', [
+  ngRoute
+])
+
 
 .controller('MyFirstController', MyFirstController)
 .filter('exclamation', ExclamationFilter)
 .filter('eldest', EldestFilter)
 .service('UserService', UserService)
 
+.config(function($routeProvider) {
+
+  $routeProvider.when('/', {
+    template: '<h1>Bienvenue</h1>'
+  })
+
+})
+
 .value('Version', '1.0.0')
+
 
 .run(function() {
   console.log('PizzaYOLO !');
